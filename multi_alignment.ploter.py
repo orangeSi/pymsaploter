de233fbfe09811347a6f60e2d4ce870bd9bf73bb
orangeSi <ilikeorangeapple@gmail.com>
2018-11-01 15:00:05 +0800

    update

--- multi_alignment.ploter.py
+++ multi_alignment.ploter.py
@@ -3,7 +3,7 @@ import re
 from collections import defaultdict
 import re
 import operator
-#from collections import OrderedDict
+import fire
 
 class AutoVivification(dict):
     """Implementation of perl's autovivification feature."""
@@ -15,13 +15,8 @@ class AutoVivification(dict):
             return value
 
 
-class multi_alignment_ploter():
-    #def __init__(self, width="100%", height="100%", x_start_shift=0, y_start_shift=0,
-    #    bp_numbers_in_one_line=60, output_svg_tag="yes", bg_color="grey",
-    #    A_font_color="black", A_bg_color="green", T_font_color="black", T_bg_color="blue",
-    #    C_font_color="black",C_bg_color="red",G_font_color="black",G_bg_color="yellow",
-    #    Null_font_color="black",Null_bg_color="white"):
-    def __init__(self, width=1000, height=1000):
+class msa_ploter():
+    def __init__(self, width=1000, height=1000, text_anchor="end"):
         self.width = width
         self.height = height
         self.svg = '''<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='%s' height='%s' > \
@@ -36,17 +31,53 @@ class multi_alignment_ploter():
         .left { stroke-dasharray: 30,10 }
         .bottom { stroke-dasharray: 20,10 }
         .right { stroke-dasharray: 10,10,20 }
-        .sample_name { text-anchor: end }
-        </style>\n''' % (width, height)
+        .sample_name { text-anchor: %s }
+        </style>\n''' % (width, height, text_anchor)
+        print ("start msa_ploter")
+    def plot(self, sort_by_file='', align='', formats='aln', types='nuc' , x_start_shift=50,
+             y_start_shift=20,
+             bp_numbers_in_one_line=100,
+             sort_list='',
+             prefix="out",
+             output_svg_tag="yes",
+             bg_color="grey",
+             A_font_color="black",
+             A_bg_color="green",
+             T_font_color="black",
+             T_bg_color="blue",
+             C_font_color="black",
+             C_bg_color="red",
+             G_font_color="black",
+             G_bg_color="yellow",
+             N_font_color="black",
+             N_bg_color="white",
+             gaps_of_fragments=3): # gaps_of_fragments =0 mean somethings
+        outname = prefix+".svg"
+        formats = str(formats)
+        types = str(types)
+        sort = self.parse_sort_file(sort_by_file, sort_list)
+        self.parse_align(align, formats, types)
+        self.plot_align(x_start_shift=x_start_shift, y_start_shift=y_start_shift,
+                        bp_numbers_in_one_line=bp_numbers_in_one_line, sort_sample_list=sort,
+                        output_svg_tag=output_svg_tag, bg_color=bg_color, A_font_color=A_font_color,
+                        A_bg_color=A_bg_color, T_font_color=T_font_color, T_bg_color=T_bg_color,
+                        C_font_color=C_font_color, C_bg_color=C_bg_color, G_font_color=G_font_color,
+                        G_bg_color=G_bg_color, N_font_color=N_font_color, N_bg_color=N_bg_color,
+                        gaps_of_fragments=gaps_of_fragments
+                        )
+        self.write2svg(outname)
 
-        print ("start multi_alignment_ploter")
+
+
     def parse_align(self, malign, formats, types):
         if formats == "aln":
-            print ("start parse aln")
+            print (f"start parse aln {types}")
+
             self.alns, self.seq_len = self.read_aln(malign, types)
-            print ("seq lenis %s " % (self.seq_len))
+            #print (f"alns is {self.alns}")
+            #print ("seq lenis %s " % (self.seq_len))
         else:
-            print (f"not support {formats}")
+            sys.exit(print (f"error:not support {formats} yet"))
     def read_aln(self, mlign, types):
         seq_len = 0
         alns = AutoVivification()
@@ -63,23 +94,32 @@ class multi_alignment_ploter():
                     if m:
                         #print ("line %s type:%s " % (line, type(line)))
                         sample_name, seq = m.group(1, 2)
+                        sample_name = sample_name.strip()
                         if re.search('a', seq): seq = re.sub('a', 'A', seq)
                         if re.search('t', seq): seq = re.sub('t', 'T', seq)
                         if re.search('c', seq): seq = re.sub('c', 'C', seq)
                         if re.search('g', seq): seq = re.sub('g', 'G', seq)
+                        if re.search('n', seq): seq = re.sub('g', 'N', seq)
                         alns[sample_name]['index'] = index
                         if not alns[sample_name]['seq']: alns[sample_name]['seq'] = ""
                         alns[sample_name]['seq'] += seq
                         if index == 0: seq_len += len(seq)
                         index += 1
+
             return alns, seq_len
         elif types == 'pep':
-            print ("not support parse aln pep")
+            sys.exit(print ("error:not support parse aln pep yet"))
+        else:
+            sys.exit(print ("error:not support parse aln {types} yet"))
     def plot_align(self, x_start_shift=0, y_start_shift=0, bp_numbers_in_one_line=1000,
                     output_svg_tag="yes", bg_color="grey",
                     A_font_color="black", A_bg_color="green", T_font_color="black", T_bg_color="blue",
                     C_font_color="black",C_bg_color="red",G_font_color="black",G_bg_color="yellow",
-                    Null_font_color="black",Null_bg_color="white", sort_samle_list=None, gaps_of_fragments=3): # gaps_of_fragments =0 mean somethings
+                    N_font_color="black",N_bg_color="white", sort_sample_list=None, gaps_of_fragments=3): # gaps_of_fragments =0 mean somethings
+        x_start_shift = int(x_start_shift)
+        y_start_shift = int(y_start_shift)
+        bp_numbers_in_one_line = int(bp_numbers_in_one_line)
+        gaps_of_fragments = int(gaps_of_fragments)
         if self.seq_len % bp_numbers_in_one_line:
             fragments = int(self.seq_len/bp_numbers_in_one_line+1)
         else:
@@ -94,8 +134,8 @@ class multi_alignment_ploter():
         self.C_bg_color = C_bg_color
         self.G_font_color = G_font_color
         self.G_bg_color = G_bg_color
-        self.Null_font_color = Null_font_color
-        self.Null_bg_color = Null_bg_color
+        self.N_font_color = N_font_color
+        self.N_bg_color = N_bg_color
 
         line_height = self.height/(fragments*(len(self.alns.keys())+ gaps_of_fragments))
         #print (f"height:{self.height};line_height:{line_height};")
@@ -122,17 +162,18 @@ class multi_alignment_ploter():
         x_start = kwargs['x_start']
         y_start = kwargs['y_start']
         sample_name_x_start = kwargs['sample_name_x_start']
-        #print (f"y_start:{y_start}")
+        padding_sample_name = 15
         sample = kwargs['sample']
         fragment = self.alns[sample]['seq'][start:end+1]
         font_color = bg_color = line = ''
-        line += f"<text x='{x_start-3}' y='{y_start+9}' class ='sample_name'>{sample}</text>"
+        max_len = 1
+        line += f"<text x='{x_start-padding_sample_name}' y='{y_start+9}' class ='sample_name'>{sample}</text>"
         for base in fragment:
             if base == "A": font_color, bg_color = self.A_font_color, self.A_bg_color
             if base == "T": font_color, bg_color = self.T_font_color, self.T_bg_color
             if base == "C": font_color, bg_color = self.C_font_color, self.C_bg_color
             if base == "G": font_color, bg_color = self.G_font_color, self.G_bg_color
-            if base == "-": font_color, bg_color = self.Null_font_color, self.Null_bg_color
+            if base == "-": font_color, bg_color = self.N_font_color, self.N_bg_color
 
             line += f"<rect  x='{x_start}' y='{y_start}' width='10'  height='10' style='fill:{bg_color};stroke:black;stroke-width:0.1;fill-opacity:0.3;stroke-opacity:0.9' /> \
             <text x='{x_start+5}' y='{y_start}' dy='9' fill='{font_color}' textLength='10'>{base}</text>\n"
@@ -149,28 +190,23 @@ class multi_alignment_ploter():
 
 
 
-def parse_sort_file(sort_file):
-    sort = []
-    if sort_file:
-        with open(sort_file) as f:
-            for line in f:
-                f = f.strip()
-                if f == "": continue
-                sort.append(f)
-    return sort
+    def parse_sort_file(self, sort_file, sort_list):
+        sort = []
+        sort_list = str(sort_list)
+        if sort_file:
+            with open(sort_file) as f:
+                for line in f:
+                    f = f.strip()
+                    if f == "": continue
+                    sort.append(f)
+        if sort == [] and sort_list:
+            if re.search(",", sort_list):
+                sort = sort_list.split(",")
+            else:
+                sys.exit(print (f"error: sort_list {sort_list}"))
+        return sort
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print ("python %s \n<mulit-alignment output from clustalw or muscle>\n<format; aln or other>\n<type;pep or nuc>\n<sample new name>\n<sort by sample name>\nwrited by myth"  % sys.argv[0])
-        sys.exit()
-    align, formats, types , *arr = sys.argv[1:]
-    new_name_file = arr[0] if len(arr) >=1 and arr[0] else ""
-    sort_file = arr[1] if len(arr) >=2 and arr[1] else ""
-    sort_sample_list = parse_sort_file(sort_file)
-
-    ploter = multi_alignment_ploter(width=1300, height=500)
-    ploter.parse_align(align, formats, types)
-    ploter.plot_align(x_start_shift=50, y_start_shift=20, bp_numbers_in_one_line=100, sort_samle_list=sort_sample_list)
-    ploter.write2svg("out.multil.svg")
+    fire.Fire(msa_ploter)
 
